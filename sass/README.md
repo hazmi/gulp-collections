5575401d82e29d0ca75ccd06ad86eb6220668627
Hazmi Abdun Nazir <hello@hazmi.id>
2015-04-18 21:55:43 +0700

    add gulp for sass

---
+++ sass/README.md
@@ -0,0 +1,19 @@
+# Gulp for development with SASS
+
+## Install
+```
+$ npm install
+```
+
+## Usage
+### Development
+```
+$ gulp
+```
+It will build and watch the changes.
+
+### Production
+```
+$ gulp build
+```
+It will minify the output.
---
+++ sass/gulpfile.js
@@ -0,0 +1,43 @@
+var gulp       = require('gulp');
+var sass       = require('gulp-sass');
+var watch      = require('gulp-watch');
+var minifyCSS  = require('gulp-minify-css');
+var rename     = require('gulp-rename');
+var header     = require('gulp-header');
+var pkg        = require('./package.json');
+
+/* Prepare banner text */
+var banner = ['/**',
+  ' * <%= pkg.name %> v<%= pkg.version %>',
+  ' * <%= pkg.description %>',
+  ' * <%= pkg.author.name %> <<%= pkg.author.email %>>',
+  ' */',
+  ''].join('\n');
+
+/* Task to compile sass */
+gulp.task('compile-sass', function() {
+  gulp.src('./assets/src/main.scss')
+    .pipe(sass())
+    .pipe(header(banner, {pkg: pkg}))
+    .pipe(gulp.dest('./assets/dist/'));
+});
+
+/* Task to watch sass changes */
+gulp.task('watch-sass', function() {
+  gulp.watch('./assets/src/**/*.scss' , ['compile-sass']);
+});
+
+/* Task to minify css */
+gulp.task('minify-css', function() {
+  gulp.src('./assets/dist/main.css')
+    .pipe(minifyCSS())
+    .pipe(header(banner, {pkg: pkg}))
+    .pipe(rename('main.min.css'))
+    .pipe(gulp.dest( './assets/dist/' ));
+});
+
+/* Task when running `gulp` from terminal */
+gulp.task('default', ['compile-sass', 'watch-sass']);
+
+/* Task when running `gulp build` from terminal */
+gulp.task('build', ['minify-css']);
